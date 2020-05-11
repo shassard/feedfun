7dfc9a9d015ebed6cfd27e7fd2db554d32b4a203
Stephen Hassard <steve@hassard.net>
2020-05-11 08:23:55 -0700

    stabalize order of articles published concurrently

--- main.go
+++ main.go
@@ -87,9 +87,14 @@ type FeedItem struct {
 // sortedFeedItems utility functions to sort a list of FeedItem
 type sortedFeedItems []*FeedItem
 
-func (i sortedFeedItems) Len() int           { return len(i) }
-func (i sortedFeedItems) Swap(x, y int)      { i[x], i[y] = i[y], i[x] }
-func (i sortedFeedItems) Less(x, y int) bool { return i[x].Published.Before(i[y].Published) }
+func (i sortedFeedItems) Len() int      { return len(i) }
+func (i sortedFeedItems) Swap(x, y int) { i[x], i[y] = i[y], i[x] }
+func (i sortedFeedItems) Less(x, y int) bool {
+	if i[x].Published.Equal(i[y].Published) {
+		return i[x].Title < i[y].Title
+	}
+	return i[x].Published.Before(i[y].Published)
+}
 
 // GetKey get the key that should be used for uniqely identifying this feed item suitable for use in a KV store
 func (i *FeedItem) GetKey() string {
